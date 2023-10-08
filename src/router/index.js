21e97749e7dc2f1cb04a617e1a401847ac89c969
Roman Turaev <romel1976@mail.ru>
2023-10-08 20:50:32 +0300

    1commit

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import MainPage from '@/views/MainPage.vue';
+import ProductPage from '@/views/ProductPage.vue';
+import CartPage from '@/views/CartPage.vue';
+import OrderPage from '@/views/OrderPage.vue';
+import OrderInfoPage from '@/views/OrderInfoPage.vue';
+import NotFoundPage from '@/views/NotFoundPage.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { name: 'main', component: MainPage, path: '/' },
+  { name: 'product', component: ProductPage, path: '/product/:id' },
+  { name: 'cart', component: CartPage, path: '/cart' },
+  { name: 'order', component: OrderPage, path: '/order' },
+  { name: 'orderInfo', component: OrderInfoPage, path: '/order/:id' },
+  { name: 'notFound', component: NotFoundPage, path: '*' },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
